3d4e3d53d93e6fcdf6998bcc2579e80ab5313461
franciscaab <franchinita10@gmail.com>
2025-07-23 13:06:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,171 @@
+const data = {
+  "Semestre 1": [
+    { nombre: "Fundamentos de la Medicina", tipo: "disciplinar" },
+    { nombre: "Biología Molecular y Genética", tipo: "básica" },
+    { nombre: "Anatomía Humana I", tipo: "básica" },
+    { nombre: "Fundamentos de la Química", tipo: "básica" },
+    { nombre: "Psicología para la atención en Salud", tipo: "básica" },
+  ],
+  "Semestre 2": [
+    { nombre: "Histoembriología I", tipo: "básica" },
+    { nombre: "Electivo I", tipo: "electiva" },
+    { nombre: "Anatomía Humana II", tipo: "básica", prerequisitos: ["Anatomía Humana I"] },
+    { nombre: "Fundamentos de Bioquímica", tipo: "básica", prerequisitos: ["Fundamentos de la Química"] },
+    { nombre: "Psicología en el Curso de la Vida", tipo: "básica", prerequisitos: ["Psicología para la atención en Salud"] },
+    { nombre: "Salud y Sociedad", tipo: "básica" },
+  ],
+   "Semestre 3": [
+    { nombre: "Histoembriología II", tipo: "básica", prerequisitos: ["Histoembriología I", "Anatomía Humana II"] },
+    { nombre: "Electivo II", tipo: "electiva" },
+    { nombre: "Neurociencias", tipo: "básica", prerequisitos: ["Anatomía Humana II"] },
+    { nombre: "Semiología I", tipo: "disciplinar", prerequisitos: ["Anatomía Humana II"] },
+    { nombre: "Fisiología Médica I", tipo: "disciplinar", prerequisitos: ["Anatomía Humana II"] },
+  ],
+   "Semestre 4": [
+    { nombre: "Métodos de investigación en Salud y Bioestadística Aplicada", tipo: "básica" },
+    { nombre: "Electivo III", tipo: "electiva" },
+    { nombre: "Semiología II", tipo: "disciplinar", prerequisitos: ["Semiología I", "Neurociencias"] },
+    { nombre: "Agentes Infecciosos I", tipo: "disciplinar", prerequisitos: ["Semiología I", "Fisiología Médica I"] },
+    { nombre: "Fisiología Médica II", tipo: "disciplinar", prerequisitos: ["Fisiología Médica I", "Histoembriología II"] },
+    { nombre: "Inmunología e Inmunización", tipo: "disciplinar", prerequisitos: ["Fisiología Médica I", "Biología Molecular y Genética"] },
+  ],
+   "Semestre 5": [
+    { nombre: "Salud Basada en la Evidencia", tipo: "básica" },
+    { nombre: "Electivo IV", tipo: "electiva" },
+    { nombre: "Atención Primaria y Alta Prevalencia", tipo: "disciplinar", prerequisitos: ["Agentes Infecciosos I", "Inmunología e Inmunización"] },
+    { nombre: "Agentes Infecciosos II", tipo: "disciplinar", prerequisitos: ["Semiología I", "Fisiología Médica I"] },
+    { nombre: "Integrado de Patología Morfofuncional I", tipo: "disciplinar", prerequisiitos: ["Fisiología Médica II", "Semiología II"] },
+    { nombre: "Farmacología I", tipo: "disciplinar", prerequisitos: ["Fisiología Médica I", "Semiología II"] },
+  ],
+   "Semestre 6": [
+    { nombre: "Medicina Interna I", tipo: "disciplinar", prerequisitos: ["Atención Primaria y Alta Prevalencia", "Integrado de Patología Morfofuncional I"] },
+    { nombre: "Fundamentos del Ecosistema de Salud Digital", tipo: "certificación" },
+    { nombre: "Integrado de Patología Morfofuncional II", tipo: "disciplinar", prerequisitos: ["Integrado de Patología Morfofuncional I", "Agentes Infecciosos II"] },
+    { nombre: "Nutrición Clínica", tipo: "disciplinar", prerequisitos: ["Integrado de Patología Morfofuncional I", "Atención Primaria y Alta Prevalencia"] },
+    { nombre: "Farmacología II", tipo: "disciplinar", prerequisitos: ["Farmacología I", "Integrado de Patología Morfofuncional I"] },
+  ],
+   "Semestre 7": [
+    { nombre: "Salud Pública y Epidemiología", tipo: "básica" },
+    { nombre: "Bioética", tipo: "certificación" },
+    { nombre: "Medicina Interna II", tipo: "disciplinar", prerequisitos: ["Medicina Interna I", "Integrado de Patología Morfofuncional II"] },
+    { nombre: "Neurología", tipo: "disciplinar", prerequisitos: ["Medicina Interna I"] },
+    { nombre: "Especialidades Médicas I", tipo: "disciplinar", prerequisitos: ["Integrado de Patología Morfofuncional II"] },
+  ],
+   "Semestre 8": [
+    { nombre: "Gestión en Sistemas de Salud", tipo: "básica" },
+    { nombre: "Cirugía", tipo: "disciplinar", prerequisitos: ["Medicina Interna II", "Farmacología II"] },
+    { nombre: "Traumatología", tipo: "disciplinar", prerequisitos: ["Medicina Interna II"] },
+    { nombre: "Especialidades Médicas II", tipo: "disciplinar", prerequisitos: ["Especialidades Médicas I"] },
+    { nombre: "Métodos de Diagnóstico Clínico", tipo: "disciplinar", prerequisitos: ["Medicina Interna II", "Nutrición Clínica"] },
+  ],
+   "Semestre 9": [
+    { nombre: "Medicina Aplicada I", tipo: "disciplinar", prerequisitos: ["Cirugía"] },
+    { nombre: "Pediatría, Traumatología y Cirugía Infantil", tipo: "disciplinar", prerequisitos: ["Especialidades Médicas II"] },
+    { nombre: "Medicina Legal", tipo: "disciplinar", prerequisitos: ["Traumatología"] },
+    { nombre: "Psiquiatría Infanto-Juvenil y del Adulto I", tipo: "disciplinar", prerequisitos: ["Neurología"] },
+    { nombre: "Oncología y Cuidados Paliativos", tipo: "disciplinar", prerequisitos: ["Métodos de Diagnóstico Clínico", "Cirugía"] },
+  ],
+   "Semestre 10": [
+    { nombre: "Medicina Aplicada II", tipo: "disciplinar", prerequisitos: ["Medicina Aplicada I"] },
+    { nombre: "Ginecología y Obstetricia", tipo: "disciplinar", prerequisitos: ["Cirugía", "Medicina Interna II"] },
+    { nombre: "Especialidades Médicas III", tipo: "disciplinar", prerequisitos: ["Especialidades Médicas II", "Traumatología", "Medicina Interna II"] },
+    { nombre: "Psiquiatría Infanto-Juvenil y del Adulto II", tipo: "disciplinar", prerequisitos: ["Psiquiatría Infanto-Juvenil y del Adulto I"] },
+    { nombre: "Inteligencia Artificial Aplicada a la Salud", tipo: "certificación" },
+  ],
+   "Semestre 11 y 12": [
+    { nombre: "Internado de Medicina Interna", tipo: "disciplinar", prerequisitos: ["Psiquiatría Infanto-Juvenil y del Adulto II", "Especialidades Médicas III", "Ginecología y Obstetricia", "Medicina Aplicada II"] },
+    { nombre: "Internado de Pediatría", tipo: "disciplinar", prerequisitos: ["Psiquiatría Infanto-Juvenil y del Adulto II", "Especialidades Médicas III", "Ginecología y Obstetricia", "Medicina Aplicada II"] },
+    { nombre: "Internado de Psiquiatría", tipo: "disciplinar", prerequisitos: ["Psiquiatría Infanto-Juvenil y del Adulto II", "Especialidades Médicas III", "Ginecología y Obstetricia", "Medicina Aplicada II"] },
+    { nombre: "Internado de Salud en Persona Mayor", tipo: "disciplinar", prerequisitos: ["Psiquiatría Infanto-Juvenil y del Adulto II", "Especialidades Médicas III", "Ginecología y Obstetricia", "Medicina Aplicada II"] },
+    { nombre: "Internado Electivo I", tipo: "disciplinar", prerequisitos: ["Psiquiatría Infanto-Juvenil y del Adulto II", "Especialidades Médicas III", "Ginecología y Obstetricia", "Medicina Aplicada II"] },
+ ],
+   "Semestre 13 y 14": [
+    { nombre: "Internado de Atención Primaria", tipo: "disciplinar", prerequisitos: ["Internado de Medicina Interna", "Internado de Pediatría", "Internado de Psiquiatría", "Internado de Salud en Persona Mayor"] },
+    { nombre: "Internado de Cirugía", tipo: "disciplinar", prerequisitos: ["Internado de Medicina Interna", "Internado de Pediatría", "Internado de Psiquiatría", "Internado de Salud en Persona Mayor"] },
+    { nombre: "Internado de Urgencia", tipo: "disciplinar", prerequisitos: ["Internado de Medicina Interna", "Internado de Pediatría", "Internado de Psiquiatría", "Internado de Salud en Persona Mayor"] },
+    { nombre: "Internado de Obstetricia y Ginecología", tipo: "disciplinar", prerequisitos: ["Internado de Medicina Interna", "Internado de Pediatría", "Internado de Psiquiatría", "Internado de Salud en Persona Mayor"] },
+    { nombre: "Internado Electivo II", tipo: "disciplinar", prerequisitos: ["Internado de Medicina Interna", "Internado de Pediatría", "Internado de Psiquiatría", "Internado de Salud en Persona Mayor"] },
+    { nombre: "Internado Electivo III", tipo: "disciplinar", prerequisitos: ["Internado de Medicina Interna", "Internado de Pediatría", "Internado de Psiquiatría", "Internado de Salud en Persona Mayor"] },
+    { nombre: "Integración para la Práctica Profesional", tipo: "disciplinar", prerequisitos: ["Internado de Medicina Interna", "Internado de Pediatría", "Internado de Psiquiatría", "Internado de Salud en Persona Mayor"] },
+ ] 
+};
+
+const estadoRamos = JSON.parse(localStorage.getItem("estadoRamos") || "{}");
+
+function guardarEstado() {
+  localStorage.setItem("estadoRamos", JSON.stringify(estadoRamos));
+}
+
+function tienePrerequisitosCumplidos(prereqs) {
+  return prereqs.every(nombre => estadoRamos[nombre] === "aprobado");
+  
+function todosLosRamosDelCiclo1Aprobados() {
+  const ramosCiclo1 = Object.entries(data)
+    .filter(([semestre]) => !semestre.includes("11") && !semestre.includes("12") && !semestre.includes("13") && !semestre.includes("14"))
+    .flatMap(([_, ramos]) => ramos);
+
+  return ramosCiclo1.every(ramo => estadoRamos[ramo.nombre] === "aprobado");
+}
+}
+
+function crearRamo(ramo, semestreContainer) {
+  const div = document.createElement("div");
+  div.className = `ramo pendiente ${ramo.tipo}`;
+  div.textContent = ramo.nombre;
+
+  const estado = estadoRamos[ramo.nombre];
+  if (estado === "aprobado") {
+    div.classList.remove("pendiente");
+    div.classList.add("aprobado");
+  }
+
+  let bloqueado = false;
+
+  if (ramo.prerequisitos && !tienePrerequisitosCumplidos(ramo.prerequisitos)) {
+    bloqueado = true;
+  }
+
+  if (ramo.nombre.includes("Internado") || ramo.nombre.includes("Integrado Electivo") || ramo.nombre.includes("Integración para la Práctica")) {
+    if (!todosLosRamosDelCiclo1Aprobados()) {
+      bloqueado = true;
+    }
+  }
+
+  if (bloqueado) {
+    div.classList.add("bloqueado");
+  } else {
+    div.addEventListener("click", () => {
+      if (div.classList.contains("aprobado")) {
+        div.classList.remove("aprobado");
+        div.classList.add("pendiente", ramo.tipo);
+        estadoRamos[ramo.nombre] = "pendiente";
+      } else {
+        div.classList.remove("pendiente", "bloqueado", ramo.tipo);
+        div.classList.add("aprobado");
+        estadoRamos[ramo.nombre] = "aprobado";
+      }
+      guardarEstado();
+      render();
+    });
+  }
+
+  semestreContainer.appendChild(div);
+}
+
+function render() {
+  const container = document.getElementById("malla");
+  container.innerHTML = "";
+
+  for (const [semestre, ramos] of Object.entries(data)) {
+    const semestreDiv = document.createElement("div");
+    semestreDiv.className = "semestre";
+    const titulo = document.createElement("h3");
+    titulo.textContent = semestre;
+    semestreDiv.appendChild(titulo);
+
+    ramos.forEach(ramo => crearRamo(ramo, semestreDiv));
+    container.appendChild(semestreDiv);
+  }
+}
+
+render();
